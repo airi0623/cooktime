5d5fbc57e52b581cb06cdcaf67591b8136f72c67
airi0623 <marcy.0623.doi@gmail.com>
2020-10-28 14:18:52 +0900

    ingredients_model修正

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,7 +1,9 @@
 class Ingredient < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :thing
+
   belongs_to :recipe,  optional: true
   #belongs_to :thing
-  belongs_to_active_hash :thing
   
   with_options presence: true do
     # validates :recipe_id
