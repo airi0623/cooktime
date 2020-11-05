a29f3496df9c5ae6856090fe0a599ce662663f13
airi0623 <marcy.0623.doi@gmail.com>
2020-11-05 14:03:27 +0900

    レシピ詳細_投稿者情報追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
-  def new
+  def show
+    @user = User.find(params[:id])
   end
 
-  def show
+  def profile
     @user = User.find(params[:id])
   end
 end
