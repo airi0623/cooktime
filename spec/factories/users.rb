3d3bceeaaea94b4bc79a19f12f0d238fb06e4e5b
airi0623 <marcy.0623.doi@gmail.com>
2020-11-20 14:40:00 +0900

    結合テスト_ユーザー編集機能

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,11 +1,12 @@
 FactoryBot.define do
   factory :user do
+    icon {[Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/factories/test_image.png'))]}
+    nickname { Faker::Name.last_name }
+    profile {Faker::Base.regexify("[aあ]{1,100}")}
+    family_name { Gimei.last.kanji }
+    first_name { Gimei.first.kanji }
     email { Faker::Internet.free_email}
     password { '1a' + Faker::Internet.password(min_length: 4) }
     password_confirmation {password}
-    nickname { Faker::Name.last_name }
-    family_name { Gimei.last.kanji }
-    first_name { Gimei.first.kanji }
-    icon {[Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/factories/test_image.png'))]}
   end
 end
