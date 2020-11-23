c80346084fd4acfaf902788f2e4c14247a6f88e4
airi0623 <marcy.0623.doi@gmail.com>
2020-11-24 01:08:37 +0900

    テストデータ作成のため

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,32 +8,32 @@
 
 require 'faker'
 
-# Thingのデータ作成
-# Thing.create(thing_name:'にんじん', kana:'にんじん',spoon:'', unit:'本')
-# Thing.create(thing_name:'大根', kana:'だいこん', spoon:'', unit:'本')
-# Thing.create(thing_name:'じゃがいも', kana:'じゃがいも', spoon:'', unit:'個')
-# Thing.create(thing_name:'ねぎ', kana:'ねぎ', spoon:'', unit:'本')
-# Thing.create(thing_name:'ブロッコリー', kana:'ぶろっこりー', spoon:'', unit:'g')
-# Thing.create(thing_name:'レモン', kana:'れもん', spoon:'', unit:'個')
-# Thing.create(thing_name:'きゅうり', kana:'きゅうり', spoon:'', unit:'本')
-# Thing.create(thing_name:'玉ねぎ', kana:'たまねぎ', spoon:'', unit:'個')
-# Thing.create(thing_name:'茄子', kana:'なすび', spoon:'', unit:'個')
-# Thing.create(thing_name:'トマト', kana:'とまと', spoon:'', unit:'個')
-# Thing.create(thing_name:'水', kana:'みず', spoon:'', unit:'ml')
-# Thing.create(thing_name:'醤油', kana:'しょうゆ', spoon:'大さじ', unit:'')
-# Thing.create(thing_name:'みりん', kana:'みりん', spoon:'大さじ', unit:'')
-# Thing.create(thing_name:'カレールー', kana:'かれーるー', spoon:'', unit:'g')
-# Thing.create(thing_name:'だし', kana:'だし', spoon:'小さじ', unit:'g')
-# Thing.create(thing_name:'鶏肉もも', kana:'とりにくもも', spoon:'', unit:'g')
-# Thing.create(thing_name:'鶏肉むね', kana:'とりにくむね', spoon:'', unit:'g')
-# Thing.create(thing_name:'豚肉肩ロース', kana:'ぶたにくかたろーす', spoon:'', unit:'g')
-# Thing.create(thing_name:'豚肉細切れ', kana:'ぶたにくこまぎれ', spoon:'', unit:'g')
-# Thing.create(thing_name:'豚肉バラ', kana:'ぶたにくばら', spoon:'', unit:'g')
-# Thing.create(thing_name:'牛肉ブロック', kana:'ぎゅうにくぶろっく', spoon:'', unit:'g')
-# Thing.create(thing_name:'牛肉細切れ', kana:'ぎゅうにくこまぎれ', spoon:'', unit:'g')
-# Thing.create(thing_name:'サーモン', kana:'さーもん', spoon:'', unit:'切')
-# Thing.create(thing_name:'豆腐', kana:'とうふ', spoon:'', unit:'切')
-# Thing.create(thing_name:'卵', kana:'たまご', spoon:'', unit:'個')
+Thingのデータ作成
+Thing.create(thing_name:'にんじん', kana:'にんじん',spoon:'', unit:'本')
+Thing.create(thing_name:'大根', kana:'だいこん', spoon:'', unit:'本')
+Thing.create(thing_name:'じゃがいも', kana:'じゃがいも', spoon:'', unit:'個')
+Thing.create(thing_name:'ねぎ', kana:'ねぎ', spoon:'', unit:'本')
+Thing.create(thing_name:'ブロッコリー', kana:'ぶろっこりー', spoon:'', unit:'g')
+Thing.create(thing_name:'レモン', kana:'れもん', spoon:'', unit:'個')
+Thing.create(thing_name:'きゅうり', kana:'きゅうり', spoon:'', unit:'本')
+Thing.create(thing_name:'玉ねぎ', kana:'たまねぎ', spoon:'', unit:'個')
+Thing.create(thing_name:'茄子', kana:'なすび', spoon:'', unit:'個')
+Thing.create(thing_name:'トマト', kana:'とまと', spoon:'', unit:'個')
+Thing.create(thing_name:'水', kana:'みず', spoon:'', unit:'ml')
+Thing.create(thing_name:'醤油', kana:'しょうゆ', spoon:'大さじ', unit:'')
+Thing.create(thing_name:'みりん', kana:'みりん', spoon:'大さじ', unit:'')
+Thing.create(thing_name:'カレールー', kana:'かれーるー', spoon:'', unit:'g')
+Thing.create(thing_name:'だし', kana:'だし', spoon:'小さじ', unit:'g')
+Thing.create(thing_name:'鶏肉もも', kana:'とりにくもも', spoon:'', unit:'g')
+Thing.create(thing_name:'鶏肉むね', kana:'とりにくむね', spoon:'', unit:'g')
+Thing.create(thing_name:'豚肉肩ロース', kana:'ぶたにくかたろーす', spoon:'', unit:'g')
+Thing.create(thing_name:'豚肉細切れ', kana:'ぶたにくこまぎれ', spoon:'', unit:'g')
+Thing.create(thing_name:'豚肉バラ', kana:'ぶたにくばら', spoon:'', unit:'g')
+Thing.create(thing_name:'牛肉ブロック', kana:'ぎゅうにくぶろっく', spoon:'', unit:'g')
+Thing.create(thing_name:'牛肉細切れ', kana:'ぎゅうにくこまぎれ', spoon:'', unit:'g')
+Thing.create(thing_name:'サーモン', kana:'さーもん', spoon:'', unit:'切')
+Thing.create(thing_name:'豆腐', kana:'とうふ', spoon:'', unit:'切')
+Thing.create(thing_name:'卵', kana:'たまご', spoon:'', unit:'個')
 
 # userのデータ作成
 # 10.times do
@@ -60,7 +60,7 @@ require 'faker'
 
 #recipeのデータ作成
 20.times do
-  user_id = Faker::Number.between(from: 1, to: 3)
+  user_id = Faker::Number.between(from: 1, to: 2)
   title = 'test_recipe'
   feature = 'テストで登録しているレシピです。テストで登録しているレシピです。テストで登録しているレシピです。テストで登録しているレシピです。テストで登録しているレシピです。テストで登録しているレシピです。テストで登録しているレシピです。'
   eat = 'テストで登録しているレシピです。テストで登録しているレシピです。テストで登録しているレシピです。テストで登録しているレシピです。テストで登録しているレシピです。テストで登録しているレシピです。テストで登録しているレシピです。'
@@ -86,21 +86,21 @@ require 'faker'
   )
 end
 
-# # Ingredientのデータ作成
-# 20.times do
-#   recipe_id = Faker::Number.between(from: 1, to: 20)
-#   thing_id = Faker::Number.between(from: 1, to: 25)
-#   name = "テスト"
-#   amount = Faker::Number.between(from: 2, to: 99)
-#   Ingredient.create!(
-#     recipe_id: recipe_id,
-#     thing_id:  thing_id,
-#     name:      name,
-#     amount:    amount,
-#     created_at:         "2020-11-01 00:00:00",
-#     updated_at:         "2020-11-01 00:00:00"
-#   )
-# end
+# Ingredientのデータ作成
+20.times do
+  recipe_id = Faker::Number.between(from: 1, to: 20)
+  thing_id = Faker::Number.between(from: 1, to: 25)
+  name = "テスト"
+  amount = Faker::Number.between(from: 2, to: 99)
+  Ingredient.create!(
+    recipe_id: recipe_id,
+    thing_id:  thing_id,
+    name:      name,
+    amount:    amount,
+    created_at:         "2020-11-01 00:00:00",
+    updated_at:         "2020-11-01 00:00:00"
+  )
+end
 
 
 
