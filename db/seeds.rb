6d5fa20d1e0bd251ca3723aa9418ab129424091d
airi0623 <marcy.0623.doi@gmail.com>
2021-01-12 12:48:27 +0900

    修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -36,34 +36,45 @@ Thing.create(thing_name:'サーモン', kana:'さーもん', spoon:'', unit:'切
 Thing.create(thing_name:'豆腐', kana:'とうふ', spoon:'', unit:'切')
 Thing.create(thing_name:'卵', kana:'たまご', spoon:'', unit:'個')
 Thing.create(thing_name:'米', kana:'こめ', spoon:'', unit:'g')
+Thing.create(thing_name:'合挽肉', kana:'あいびきにく', spoon:'', unit:'g')
+Thing.create(thing_name:'パン粉', kana:'ぱんこ', spoon:'', unit:'g')
+Thing.create(thing_name:'ピーマン', kana:'ぴーまん', spoon:'', unit:'個')
 
-# userのデータ作成
-3.times do
-  icon = open('./db/fixtures/icon.png')
-  nickname  = Faker::Name.last_name 
-  profile = Faker::Base.regexify("[aあ]{1,100}")
-  # family_name = Gimei.last.kanji
-  family_name = '料理'
-  # first_name = Gimei.first.kanji
-  first_name = '花子'
-  email = Faker::Internet.free_email
-  password = '1a1a1a1a'
-  User.create!(
-    icon:         icon,
-    profile:      profile,
-    nickname:     nickname,
-    family_name:  family_name, 
-    first_name:   first_name, 
-    email:        email, 
-    password:     password, 
-    encrypted_password: password,
-    created_at:         "2020-11-01 00:00:00",
-    updated_at:         "2020-11-01 00:00:00"
-  )
-end
+# # userのデータ作成
+User.create(icon: open('./db/fixtures/icon_1.png'), profile:'テストユーザーです。料理と猫が好きです！私もみんなに見てもらえるレシピ投稿頑張ります！', nickname:'シャム猫花子', family_name:'シャム', first_name:'猫花子', email: 's@s', password: '1a1a1a1a', encrypted_password: '1a1a1a1a')
+User.create(icon: open('./db/fixtures/icon_2.png'), profile:'テストユーザーです。料理と猫が好きです！私もみんなに見てもらえるレシピ投稿頑張ります！', nickname:'黒猫太郎',family_name:'黒猫', first_name:'太郎', email: 'k@k', password: '1a1a1a1a', encrypted_password: '1a1a1a1a')
+User.create(icon: open('./db/fixtures/icon_3.png'), profile:'テストユーザーです。料理と猫が好きです！私もみんなに見てもらえるレシピ投稿頑張ります！', nickname:'猫二郎',family_name:'猫', first_name:'二郎', email: 'n@n', password: '1a1a1a1a', encrypted_password: '1a1a1a1a')
+# 3.times do
+#   icon =  ('./db/fixtures/icon.png')
+#   nickname  = Faker::Name.last_name 
+#   profile = "テストユーザーです。料理と猫が好きです！私もみんなに見てもらえるレシピ投稿頑張ります！"
+#   # family_name = Gimei.last.kanji
+#   family_name = 'シャム'
+#   # first_name = Gimei.first.kanji
+#   first_name = '猫花子'
+#   email = Faker::Internet.free_email
+#   password = '1a1a1a1a'
+#   User.create!(
+#     icon:         icon,
+#     profile:      profile,
+#     nickname:     nickname,
+#     family_name:  family_name, 
+#     first_name:   first_name, 
+#     email:        email, 
+#     password:     password, 
+#     encrypted_password: password,
+#     created_at:         "2020-11-01 00:00:00",
+#     updated_at:         "2020-11-01 00:00:00"
+#   )
+# end
+
+#recipeのデータ作成
+Recipe.create(images:[open('./db/fixtures/img_1.png'), open('./db/fixtures/img_2.png')], profile:'テストユーザーです。料理と猫が好きです！私もみんなに見てもらえるレシピ投稿頑張ります！', nickname:'シャム猫花子', family_name:'シャム', first_name:'猫花子', email: 's@s', password: '1a1a1a1a', encrypted_password: '1a1a1a1a')
 
 #recipeのデータ作成
 20.times do
+  n = 1
+  i = n % 5
   user_id = Faker::Number.between(from: 1, to: 2)
   title = 'レシピタイトル'
   feature = 'テストで登録しているレシピです。テストで登録しているレシピです。テストで登録しているレシピです。テストで登録しているレシピです。テストで登録しているレシピです。テストで登録しているレシピです。テストで登録しているレシピです。'
@@ -73,7 +84,7 @@ end
   frozen_date = Faker::Number.between(from: 5, to: 10)
   time  = Faker::Number.between(from: 10, to: 30)
   text = 'テストで登録しているレシピです。テストで登録しているレシピです。テストで登録しているレシピです。テストで登録しているレシピです。テストで登録しているレシピです。テストで登録しているレシピです。テストで登録しているレシピです。'
-  images = [open('./db/fixtures/test.png')]
+  images = [open("./db/fixtures/img_#{i}.jpg"), open("./db/fixtures/ing_1.jpg")]
   Recipe.create!(
     user_id:      user_id,
     title:        title,
@@ -88,6 +99,7 @@ end
     created_at:   "2020-11-01 00:00:00",
     updated_at:   "2020-11-01 00:00:00"
   )
+  n += 1
 end
 
 # Ingredientのデータ作成
@@ -122,7 +134,7 @@ Like.create(user_id: 3, recipe_id: 4)
 10.times do
   user_id = Faker::Number.between(from: 1, to: 2)
   recipe_id = Faker::Number.between(from: 1, to: 5)
-  repo_image = open('./db/fixtures/test.png')
+  repo_image =  ('./db/fixtures/test.png')
   repo = Faker::Base.regexify("[aあ]{1,100}")
   Report.create!(
     user_id:     user_id,
